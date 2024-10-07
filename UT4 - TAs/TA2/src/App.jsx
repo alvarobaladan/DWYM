b6a85cdfbf5157b305ae6cfe5eef20b685cb46c3
Álvaro <alvarobaladan@gmail.com>
2024-10-06 21:46:25 -0300

    Agrega TA2

---
+++ UT4 - TAs/TA2/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Card from './Card';
+
+function App() {
+  return (
+    <div>
+      <Card>
+          <p><b>Titulo: </b>"Título de la tarea 1"</p>
+          <p><b>Descripcion: </b>"Ésta es la descripción de la tarea 1."</p>
+          <p><b>Persona Asignada: </b>"Álvaro"</p>
+          <p><b>Fecha Inicio: </b>"2024-09-17"</p>
+          <p><b>Fecha Fin: </b>"2024-09-18"</p>
+      </Card>
+      <Card>
+          <p><b>Titulo: </b>"Título de la tarea 2"</p>
+          <p><b>Descripcion: </b>"Ésta es la descripción de la tarea 2."</p>
+          <p><b>Persona Asignada: </b>"Álvaro"</p>
+          <p><b>Fecha Inicio: </b>"2024-09-20"</p>
+          <p><b>Fecha Fin: </b>"2024-10-01"</p>
+      </Card>
+      <Card>
+          <p><b>Titulo: </b>"Título de la tarea 3"</p>
+          <p><b>Descripcion: </b>"Ésta es la descripción de la tarea 3."</p>
+          <p><b>Persona Asignada: </b>"Álvaro"</p>
+          <p><b>Fecha Inicio: </b>"2024-09-21"</p>
+          <p><b>Fecha Fin: </b>"2024-11-01"</p>
+      </Card>
+    </div>
+  )
+}
+
+export default App
---
+++ UT4 - TAs/TA2/src/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+function Card({ children }){
+    return(
+      <>
+        <div class="card">
+          {children}
+        </div>
+      </>
+    );
+  }
+
+  export default Card;
+
